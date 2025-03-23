f69d57da4e27635fda3d0bd161676ba263ffff48
Jesús Alós <alosjesus13@gmail.com>
2025-03-23 12:14:25 +0000

    generadorNombresDom

--- src/app.js
+++ src/app.js
@@ -1,25 +1,15 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
+let resultado = '';
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  
-  let who = ['El perro ', 'Mi hermana ', 'Mi mujer ', 'Mi cuñado '];
-  let action = ['se comió ', 'rompió ', 'perdió ', 'estropeó ', 'olvidó '];
-  let what = ['mis deberes ', 'mi teléfono ', 'mi coche ', 'mi moto '];
-  let when = ['antes de clase. ', 'mientras estaba durmiendo. ', 'cuando estaba en el gimnasio. ', 'mientras hacía ejercicio. ', 'cuando salía de casa.'];
-
- 
-  let rdm1 = Math.floor(Math.random() * who.length);
-  let rdm2 = Math.floor(Math.random() * action.length);
-  let rdm3 = Math.floor(Math.random() * what.length);
-  let rdm4 = Math.floor(Math.random() * when.length);
-  
-
-
-  document.querySelector("#excuse").innerHTML =who[rdm1] + action[rdm2] + what[rdm3] + when[rdm4];
-  
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      resultado += pronoun[p] + adj[a] + noun[n] + '.com' + '<hr>';
+    }
+  }
 }
+
+document.getElementById('dominio').innerHTML = resultado;
